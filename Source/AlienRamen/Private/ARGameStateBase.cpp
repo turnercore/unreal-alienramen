dbd6267f62634712c1de4b924eaaa75f16c75da4
Turner Monroe <me@turnercore.dev>
2026-02-27 23:30:23 +0100

    Add player access convenience functions to AARGameStateBase for slot and context resolution

--- Source/AlienRamen/Private/ARGameStateBase.cpp
+++ Source/AlienRamen/Private/ARGameStateBase.cpp
@@ -4,6 +4,8 @@
 #include "ARPlayerStateBase.h"
 #include "ARSaveSubsystem.h"
 #include "Engine/GameInstance.h"
+#include "GameFramework/Pawn.h"
+#include "GameFramework/PlayerController.h"
 #include "Net/UnrealNetwork.h"
 
 AARGameStateBase::AARGameStateBase()
@@ -73,6 +75,73 @@ bool AARGameStateBase::RemoveTrackedPlayer(AARPlayerStateBase* Player)
 bool AARGameStateBase::ContainsTrackedPlayer(const AARPlayerStateBase* Player) const
 {
 	return Player && Players.Contains(const_cast<AARPlayerStateBase*>(Player));
+}
+
+AARPlayerStateBase* AARGameStateBase::GetPlayerBySlot(EARPlayerSlot Slot) const
+{
+	for (AARPlayerStateBase* Player : Players)
+	{
+		if (IsValid(Player) && Player->GetPlayerSlot() == Slot)
+		{
+			return Player;
+		}
+	}
+
+	return nullptr;
+}
+
+AARPlayerStateBase* AARGameStateBase::GetOtherPlayerStateFromPlayerState(const AARPlayerStateBase* CurrentPlayerState) const
+{
+	for (AARPlayerStateBase* Player : Players)
+	{
+		if (!IsValid(Player) || Player == CurrentPlayerState)
+		{
+			continue;
+		}
+		return Player;
+	}
+
+	return nullptr;
+}
+
+AARPlayerStateBase* AARGameStateBase::GetOtherPlayerStateFromController(const APlayerController* CurrentPlayerController) const
+{
+	if (!CurrentPlayerController)
+	{
+		return GetOtherPlayerStateFromPlayerState(nullptr);
+	}
+
+	return GetOtherPlayerStateFromPlayerState(CurrentPlayerController->GetPlayerState<AARPlayerStateBase>());
+}
+
+AARPlayerStateBase* AARGameStateBase::GetOtherPlayerStateFromPawn(const APawn* CurrentPlayerPawn) const
+{
+	if (!CurrentPlayerPawn)
+	{
+		return GetOtherPlayerStateFromPlayerState(nullptr);
+	}
+
+	return GetOtherPlayerStateFromPlayerState(CurrentPlayerPawn->GetPlayerState<AARPlayerStateBase>());
+}
+
+AARPlayerStateBase* AARGameStateBase::GetOtherPlayerStateFromContext(const UObject* CurrentPlayerContext) const
+{
+	if (const AARPlayerStateBase* AsPS = Cast<AARPlayerStateBase>(CurrentPlayerContext))
+	{
+		return GetOtherPlayerStateFromPlayerState(AsPS);
+	}
+
+	if (const APlayerController* AsPC = Cast<APlayerController>(CurrentPlayerContext))
+	{
+		return GetOtherPlayerStateFromController(AsPC);
+	}
+
+	if (const APawn* AsPawn = Cast<APawn>(CurrentPlayerContext))
+	{
+		return GetOtherPlayerStateFromPawn(AsPawn);
+	}
+
+	return GetOtherPlayerStateFromPlayerState(nullptr);
 }
 
 void AARGameStateBase::OnRep_Players()
