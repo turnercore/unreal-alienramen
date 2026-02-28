bbb4af635927a90f55bcd6ad5fddffb904170db7
turnercore <me@turnercore.dev>
2026-02-28 18:43:13 +0100

    Added game version checks to connecting players so tmake sure they only connect to compatible sessions
    We should also add a session watermark on the main menu

--- Source/AlienRamen/Private/ARGameInstance.cpp
+++ Source/AlienRamen/Private/ARGameInstance.cpp
@@ -2,6 +2,7 @@
 
 #include "ARLog.h"
 #include "ARSaveSubsystem.h"
+#include "OnlineSessionSettings.h"
 
 void UARGameInstance::Init()
 {
@@ -22,6 +23,38 @@ UARSaveSubsystem* UARGameInstance::GetARSaveSubsystem() const
 	return GetSubsystem<UARSaveSubsystem>();
 }
 
+// ---- Network compatibility helpers ----
+
+namespace
+{
+	// Session setting key advertised so clients can pre-filter incompatible sessions.
+	static const FName SessionSetting_ARProtocol(TEXT("ARProtocol"));
+}
+
+bool UARGameInstance::IsARProtocolCompatible(const int32 OtherProtocol)
+{
+	return OtherProtocol >= ARMinCompatibleProtocol && OtherProtocol <= ARProtocolVersion;
+}
+
+void UARGameInstance::ApplyARProtocolSessionSetting(FOnlineSessionSettings& SessionSettings)
+{
+	SessionSettings.Set(SessionSetting_ARProtocol, ARProtocolVersion, EOnlineDataAdvertisementType::ViaOnlineServiceAndPing);
+}
+
+bool UARGameInstance::GetARProtocolFromSession(const FOnlineSessionSearchResult& SearchResult, int32& OutProtocol)
+{
+	OutProtocol = 0;
+
+	int32 ProtocolValue = 0;
+	if (SearchResult.Session.SessionSettings.Get(SessionSetting_ARProtocol, ProtocolValue))
+	{
+		OutProtocol = ProtocolValue;
+		return true;
+	}
+
+	return false;
+}
+
 void UARGameInstance::BP_OnARGameInstanceInitialized_Implementation()
 {
 }
@@ -29,4 +62,3 @@ void UARGameInstance::BP_OnARGameInstanceInitialized_Implementation()
 void UARGameInstance::BP_OnARGameInstanceShutdown_Implementation()
 {
 }
-
