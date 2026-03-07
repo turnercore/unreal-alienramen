706d7502a1591b7bae306d404d7bca90be5aa9a9
turnercore <me@turnercore.dev>
2026-03-07 08:26:27 +0100

    Broadcast find failure when busy

--- Source/AlienRamen.Target.cs
+++ Source/AlienRamen.Target.cs
@@ -7,6 +7,10 @@ public class AlienRamenTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V6;
+		GlobalDefinitions.Add("UE_PROJECT_STEAMPRODUCTNAME=\"480\"");
+		GlobalDefinitions.Add("UE_PROJECT_STEAMGAMEDIR=\"alienramen\"");
+		GlobalDefinitions.Add("UE_PROJECT_STEAMGAMEDESC=\"Alien Ramen\"");
+		GlobalDefinitions.Add("UE_PROJECT_STEAMSHIPPINGID=480");
 
 		ExtraModuleNames.AddRange( new string[] { "AlienRamen" } );
 	}
