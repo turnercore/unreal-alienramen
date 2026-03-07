8d2eaac1ff4f0f499e834d1270d5c0bf41316854
turnercore <me@turnercore.dev>
2026-03-07 08:37:03 +0100

    Finish Steam SDK integration

--- Source/AlienRamen.Target.cs
+++ Source/AlienRamen.Target.cs
@@ -1,5 +1,6 @@
 using UnrealBuildTool;
 using System.Collections.Generic;
+using System;
 
 public class AlienRamenTarget : TargetRules
 {
@@ -7,10 +8,35 @@ public class AlienRamenTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V6;
-		GlobalDefinitions.Add("UE_PROJECT_STEAMPRODUCTNAME=\"480\"");
-		GlobalDefinitions.Add("UE_PROJECT_STEAMGAMEDIR=\"alienramen\"");
-		GlobalDefinitions.Add("UE_PROJECT_STEAMGAMEDESC=\"Alien Ramen\"");
-		GlobalDefinitions.Add("UE_PROJECT_STEAMSHIPPINGID=480");
+
+		string steamShippingId = Environment.GetEnvironmentVariable("UE_PROJECT_STEAMSHIPPINGID");
+		if (string.IsNullOrWhiteSpace(steamShippingId))
+		{
+			steamShippingId = "480";
+		}
+
+		string steamProductName = Environment.GetEnvironmentVariable("UE_PROJECT_STEAMPRODUCTNAME");
+		if (string.IsNullOrWhiteSpace(steamProductName))
+		{
+			steamProductName = steamShippingId;
+		}
+
+		string steamGameDir = Environment.GetEnvironmentVariable("UE_PROJECT_STEAMGAMEDIR");
+		if (string.IsNullOrWhiteSpace(steamGameDir))
+		{
+			steamGameDir = "alienramen";
+		}
+
+		string steamGameDesc = Environment.GetEnvironmentVariable("UE_PROJECT_STEAMGAMEDESC");
+		if (string.IsNullOrWhiteSpace(steamGameDesc))
+		{
+			steamGameDesc = "Alien Ramen";
+		}
+
+		GlobalDefinitions.Add("UE_PROJECT_STEAMPRODUCTNAME=\"" + steamProductName + "\"");
+		GlobalDefinitions.Add("UE_PROJECT_STEAMGAMEDIR=\"" + steamGameDir + "\"");
+		GlobalDefinitions.Add("UE_PROJECT_STEAMGAMEDESC=\"" + steamGameDesc + "\"");
+		GlobalDefinitions.Add("UE_PROJECT_STEAMSHIPPINGID=" + steamShippingId);
 
 		ExtraModuleNames.AddRange( new string[] { "AlienRamen" } );
 	}
