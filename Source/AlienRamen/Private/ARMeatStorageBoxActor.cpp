72a5f183ee6933515797f5ae53838ece70026662
Turner Monroe <me@turnercore.dev>
2026-03-10 19:43:34 +0100

    feat: Implement shop dispenser actor for item dispensing and integrate with player controller

--- Source/AlienRamen/Private/ARMeatStorageBoxActor.cpp
+++ Source/AlienRamen/Private/ARMeatStorageBoxActor.cpp
@@ -1,92 +1,40 @@
 #include "ARMeatStorageBoxActor.h"
 
-#include "ARGameStateBase.h"
 #include "ARPlayerController.h"
 #include "ARRamenMeatActor.h"
-#include "ARSaveTypes.h"
-#include "ARShopCarryComponent.h"
-#include "Components/SceneComponent.h"
-#include "GameFramework/Pawn.h"
-
-namespace
-{
-	static int32* ResolveMeatBucket(FARMeatState& MeatState, const EARAffinityColor Color)
-	{
-		switch (Color)
-		{
-		case EARAffinityColor::Red:
-			return &MeatState.RedAmount;
-		case EARAffinityColor::Blue:
-			return &MeatState.BlueAmount;
-		case EARAffinityColor::White:
-			return &MeatState.WhiteAmount;
-		default:
-			return &MeatState.UnspecifiedAmount;
-		}
-	}
-}
 
 AARMeatStorageBoxActor::AARMeatStorageBoxActor()
 {
-	bReplicates = true;
+}
 
-	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
-	SetRootComponent(SceneRoot);
-
-	SpawnAnchor = CreateDefaultSubobject<USceneComponent>(TEXT("SpawnAnchor"));
-	SpawnAnchor->SetupAttachment(SceneRoot);
+void AARMeatStorageBoxActor::BeginPlay()
+{
+	Super::BeginPlay();
+	SyncLegacyDefinition();
 }
 
 bool AARMeatStorageBoxActor::TryDispenseMeat(AARPlayerController* RequestingController)
 {
-	if (!HasAuthority())
+	SyncLegacyDefinition();
+	return TryDispenseToController(RequestingController, MeatItemTag);
+}
+
+void AARMeatStorageBoxActor::SyncLegacyDefinition()
+{
+	FARShopDispenseDefinition Definition;
+	Definition.ItemTag = MeatItemTag;
+	Definition.SpawnActorClass = MeatActorClass;
+	Definition.AmountPerDispense = FMath::Max(1, MeatAmountPerDispense);
+	Definition.SourceType = EARShopDispenserSourceType::GameStateMeatReserve;
+	Definition.SourceColor = MeatColor;
+	Definition.bAutoPlaceIntoCarry = true;
+
+	if (DispenseDefinitions.Num() <= 0)
 	{
-		return false;
+		DispenseDefinitions.Add(Definition);
+		return;
 	}
 
-	AARGameStateBase* ARGameState = GetWorld() ? GetWorld()->GetGameState<AARGameStateBase>() : nullptr;
-	if (!ARGameState)
-	{
-		return false;
-	}
-
-	FARMeatState MeatState = ARGameState->GetMeat();
-	int32* Bucket = ResolveMeatBucket(MeatState, MeatColor);
-	const int32 AmountToDispense = FMath::Max(1, MeatAmountPerDispense);
-	if (!Bucket || *Bucket < AmountToDispense)
-	{
-		return false;
-	}
-
-	*Bucket -= AmountToDispense;
-	ARGameState->SetMeatFromSave(MeatState);
-
-	TSubclassOf<AARRamenMeatActor> SpawnClass = MeatActorClass;
-	if (!SpawnClass)
-	{
-		SpawnClass = AARRamenMeatActor::StaticClass();
-	}
-
-	const FVector SpawnLocation = SpawnAnchor ? SpawnAnchor->GetComponentLocation() : GetActorLocation();
-	const FRotator SpawnRotation = SpawnAnchor ? SpawnAnchor->GetComponentRotation() : GetActorRotation();
-	FActorSpawnParameters SpawnParams;
-	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
-	AARRamenMeatActor* SpawnedMeat = GetWorld()->SpawnActor<AARRamenMeatActor>(SpawnClass, SpawnLocation, SpawnRotation, SpawnParams);
-	if (!SpawnedMeat)
-	{
-		*Bucket += AmountToDispense;
-		ARGameState->SetMeatFromSave(MeatState);
-		return false;
-	}
-
-	SpawnedMeat->SetMeatData(MeatColor, AmountToDispense);
-
-	APawn* RequestingPawn = RequestingController ? RequestingController->GetPawn() : nullptr;
-	UARShopCarryComponent* CarryComponent = RequestingPawn ? RequestingPawn->FindComponentByClass<UARShopCarryComponent>() : nullptr;
-	if (CarryComponent && !CarryComponent->HasHeldActor())
-	{
-		CarryComponent->TrySetHeldActor(SpawnedMeat);
-	}
-
-	return true;
+	DispenseDefinitions.SetNum(1, EAllowShrinking::No);
+	DispenseDefinitions[0] = Definition;
 }
--- Source/AlienRamen/Private/ARShopPlayerController.cpp
+++ Source/AlienRamen/Private/ARShopPlayerController.cpp
@@ -1,5 +1,28 @@
 #include "ARShopPlayerController.h"
 
+#include "ARShopDispenserActor.h"
+
 AARShopPlayerController::AARShopPlayerController()
 {
+}
+
+void AARShopPlayerController::RequestShopDispenseFromDispenser(AARShopDispenserActor* DispenserActor, const FGameplayTag ItemTag)
+{
+	if (!DispenserActor)
+	{
+		return;
+	}
+
+	if (HasAuthority())
+	{
+		DispenserActor->TryDispenseToController(this, ItemTag);
+		return;
+	}
+
+	ServerRequestShopDispenseFromDispenser(DispenserActor, ItemTag);
+}
+
+void AARShopPlayerController::ServerRequestShopDispenseFromDispenser_Implementation(AARShopDispenserActor* DispenserActor, const FGameplayTag ItemTag)
+{
+	RequestShopDispenseFromDispenser(DispenserActor, ItemTag);
 }
